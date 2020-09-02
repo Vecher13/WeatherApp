e59f77c94240aa6e2aaa33573fec9c774a295aa1
Ash <ashraf97@yandex.ru>
2020-09-02 22:12:16 +0300

    Initial Commit

---
+++ almofireWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  almofireWeather
+//
+//  Created by Ash on 02.09.2020.
+//  Copyright © 2020 Ash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
