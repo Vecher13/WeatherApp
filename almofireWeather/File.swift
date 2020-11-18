f97f4234392d0f4f2e7d1aac8bf4e954a06d1697
Ash <ashraf97@yandex.ru>
2020-11-19 00:21:03 +0300

    Some changes.

--- almofireWeather/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  almofireWeather
-//
-//  Created by Ash on 24.09.2020.
-//  Copyright © 2020 Ash. All rights reserved.
-//
-
-import Foundation
---
+++ almofireWeather/Modal/IconsKey.swift
@@ -0,0 +1,118 @@
+//
+//  IconsKey.swift
+//  
+//
+//  Created by Ash on 11.09.2020.
+//
+
+import Foundation
+
+
+
+func iconCode(code: Int) -> String {
+   var iconId:String = ""
+    switch code {
+    case 0:
+        iconId = "\u{f056}"
+    case 1:
+         iconId = "\u{f00e}"
+        case 2:
+         iconId = "\u{f073}"
+        case 3:
+         iconId = "\u{f01e}"
+        case 4:
+         iconId = "\u{f01e}"
+        case 5:
+         iconId = "\u{f017}"
+        case 6:
+         iconId = "\u{f017}"
+        case 7:
+         iconId = "\u{f017}"
+        case 8:
+         iconId = "\u{f015}"
+        case 9:
+         iconId = "\u{f01a}"
+        case 10:
+         iconId = "\u{f015}"
+        case 11:
+         iconId = "\u{f01a}"
+        case 12:
+         iconId = "\u{f01a}"
+        case 13:
+         iconId = "\u{f01b}"
+        case 14:
+         iconId = "\u{f00a}"
+        case 15:
+         iconId = "\u{f064}"
+        case 16:
+         iconId = "\u{f01b}"
+        case 17:
+         iconId = "\u{f01b}"
+        case 18:
+         iconId = "\u{f017}"
+        case 19:
+         iconId = "\u{f063}"
+        case 20:
+         iconId = "\u{f014}"
+        case 21:
+         iconId = "\u{f021}"
+        case 22:
+         iconId = "\u{f062}"
+        case 23:
+         iconId = "\u{f050}"
+        case 24:
+         iconId = "\u{f050}"
+        case 25:
+         iconId = "\u{f050}"
+        case 26:
+         iconId = "\u{f013}"
+        case 27:
+         iconId = "\u{f031}"
+        case 28:
+         iconId = "\u{f002}"
+        case 29:
+         iconId = "\u{f031}"
+        case 30:
+         iconId = "\u{f002}"
+        case 31:
+         iconId = "\u{f02e}"
+        case 32:
+         iconId = "\u{f00d}"
+        case 33:
+         iconId = "\u{f083}"
+        case 34:
+         iconId = "\u{f00c}"
+        case 35:
+         iconId = "\u{f017}"
+        case 36:
+         iconId = "\u{f072}"
+        case 37:
+         iconId = "\u{f00e}"
+        case 38:
+         iconId = "\u{f00e}"
+        case 39:
+         iconId = "\u{f00e}"
+        case 40:
+         iconId = "\u{f01a}"
+        case 41:
+         iconId = "\u{f064}"
+        case 42:
+         iconId = "\u{f01b}"
+        case 43:
+         iconId = "\u{f064}"
+        case 44:
+         iconId = "\u{f00c}"
+        case 45:
+         iconId = "\u{f00e}"
+        case 46:
+         iconId = "\u{f01b}"
+        case 47:
+         iconId = "\u{f00e}"
+        
+    default:
+        iconId = "\u{f077}"
+    }
+    return iconId
+}
+
+
